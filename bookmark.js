cbdcea7e26ccd4054ec1495937675979332fa481
mayank <mayank08012003@gmail.com>
2024-06-27 13:34:57 +0530

    web dev practice

---
+++ bookmark.js
@@ -0,0 +1,1 @@
+alert(" here are your favourite sites")
---
+++ first.js
@@ -0,0 +1,4 @@
+console.log("hello world!!!");
+    alert("website got hacked");
+    document.write("world hello");
+    document.getElementById("second").innerHTML="my name is mayank";
